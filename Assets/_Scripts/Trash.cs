d9f3eaddfd65636675fcef6bff06c286015cd091
Shyplants <wlsdn1004100@naver.com>
2023-06-02 17:26:20 +0900

    TrashType 추가
    Trash 스크립트를 통해 쓰레기 오브젝트에 TrashType 설정

--- Assets/_Scripts/Trash.cs
+++ Assets/_Scripts/Trash.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TrashType
+{
+    General,                    // 일반 쓰레기
+    Paper,                      // 종이류
+    PaperPack,                  // 종이팩
+    MetalCan,                   // 금속캔
+    GlassBottle,                // 유리병
+    PlasticBottle,              // 페트병
+    PlasticContainer,           // 플라스틱 용기
+    VinylProuduct               // 비닐류
+}
+
 public class Trash : MonoBehaviour
 {
-    public GameObject[] trashPrefabs;                     // 생성할 쓰레기의 프리팹
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TrashType trashType;
 }
